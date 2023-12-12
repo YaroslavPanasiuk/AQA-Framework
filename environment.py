171d7410e168882f0ba7b3cc84bbfa8dbd8f7b49
yaros <yaroslav.panasiuk.1902@gmail.com>
2023-12-12 13:22:22 +0200

    13

--- environment.py
+++ environment.py
@@ -7,10 +7,10 @@ import sys
 def before_all(context: Context):
     #sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8')
     chrome_options = Options()
-    chrome_options.binary_location = r"C:\Drivers\chrome-win64\chrome.exe"
+    chrome_options.binary_location = r"/usr/bin/chrome"#r"C:\Drivers\chrome-win64\chrome.exe"
     firefox_options = Options()
     firefox_options.binary_location = r"/usr/bin/firefox"
-    context.driver = webdriver.Firefox(firefox_options)
+    context.driver = webdriver.Chrome(chrome_options)
 
 
 def after_all(context: Context):
