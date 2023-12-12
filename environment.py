f3ea7dbb137ebf29cc5b0efefc0ce134ecf055e6
yaros <yaroslav.panasiuk.1902@gmail.com>
2023-12-12 14:10:57 +0200

    21

--- environment.py
+++ environment.py
@@ -9,6 +9,7 @@ def before_all(context: Context):
     #sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8')
     chrome_options = webdriver.chrome.options.Options()
     chrome_options.binary_location = r"/usr/bin/google-chrome-stable"#r"C:\Drivers\chrome-win64\chrome.exe"
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     firefox_options = webdriver.firefox.options.Options()
     firefox_options.binary_location = r"/usr/bin/firefox"
