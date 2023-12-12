224898b72ab2e82aa4b0d6a9c9bf106bd5689851
yaros <yaroslav.panasiuk.1902@gmail.com>
2023-12-12 14:01:30 +0200

    19

--- environment.py
+++ environment.py
@@ -1,6 +1,7 @@
 from behave.runner import Context
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 import sys
 
 
@@ -9,14 +10,12 @@ def before_all(context: Context):
     chrome_options = webdriver.chrome.options.Options()
     chrome_options.binary_location = r"/usr/bin/google-chrome-stable"#r"C:\Drivers\chrome-win64\chrome.exe"
 
-    #firefox_options = webdriver.FirefoxOptions
-    #firefox_options.binary_location = r"/usr/bin/firefox"
-    context.driver = webdriver.Firefox()
-
+    firefox_options = webdriver.firefox.options.Options()
+    firefox_options.binary_location = r"/usr/bin/firefox"
+    context.driver = webdriver.Firefox(options=firefox_options)
 
 
 def after_all(context: Context):
-    return
     context.driver.quit()
 
 
